54521428449d45e98db24db1adb75d0f28dcd661
sadkingo <hker0004@gmail.com>
2024-12-27 23:26:42 +0100

    Fixed path

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -2,7 +2,7 @@
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
-import { themeSystem } from "@/theme";
+import { themeSystem } from "@/config/theme";
 
 export function Provider(props: ColorModeProviderProps) {
   return (
