8b99428252ff53f296c64bf6937a36e07216d9b4
sadkingo <hker0004@gmail.com>
2024-12-28 18:43:20 +0100

    Created bottom navigation bar

---
+++ components/bottom-nav/BottomNav.component.tsx
@@ -0,0 +1,53 @@
+import { HStack } from "@chakra-ui/react";
+import React from "react";
+import Menu from "../menu";
+import { Icon } from "@iconify/react";
+import Link from "next/link";
+import colors from "@/config/colors";
+// TODO Grab the menus form API
+const BottomNav = () => {
+  return (
+    <HStack
+      bg={{ _light: colors.mediumLight, _dark: colors.medium }}
+      mdDown={{ display: "none" }}
+      p={1}
+      borderBottomWidth={1}
+      justifyContent="space-around"
+      fontSize={18}
+    >
+      <HStack gap={8}>
+        <Menu
+          title="Categories"
+          items={[{ id: 0, title: "All products", url: "/" }]}
+        >
+          <Icon icon="ep:arrow-down" />
+        </Menu>
+        <Link href="/about-us">About us</Link>
+        <Link href="/contact-us">Contact Us</Link>
+      </HStack>
+      <HStack gap={8} justifyContent="center" alignItems="center">
+        <Menu
+          items={[
+            {
+              id: 0,
+              title: "Arabic",
+              content: (
+                <HStack fontSize={16}>
+                  <Icon icon="twemoji:flag-algeria" />
+                  Arabic
+                </HStack>
+              ),
+              url: "/",
+            },
+          ]}
+        >
+          <Icon icon="flag:us-4x3" /> English
+        </Menu>
+        <Link href="/about-us">Privacy Policy</Link>
+        <Link href="/contact-us">Terms & Conditions</Link>
+      </HStack>
+    </HStack>
+  );
+};
+
+export default BottomNav;
---
+++ components/bottom-nav/BottomNav.container.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import BottomNav from "./BottomNav.component";
+
+const BottomNavContainer = () => {
+  return <BottomNav />;
+};
+
+export default BottomNavContainer;
