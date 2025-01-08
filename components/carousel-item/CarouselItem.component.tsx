76bf4013e3206d3535d9fa44bfc16006ccb7c132
sadkingo <hker0004@gmail.com>
2025-01-08 03:11:57 +0100

    Added carousel

--- components/carousel-item/CarouselItem.component.tsx
+++ components/carousel-item/CarouselItem.component.tsx
@@ -1,41 +1,59 @@
 import colors from '@/config/colors'
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 import React, { FC } from 'react'
 
 const CarouselItem: FC<CarouselItemProps> = ({ image }) => {
     return (
-        <Box className="relative">
+        <Box className="relative px-3">
             <Image
                 src={image}
-                className="aspect-[2/1] rounded-xl"
+                className="rounded-xl aspect-[2/1]"
                 width="1200"
                 height="600"
                 alt={`Fruit`}
             />
             <Flex
+                pos="absolute"
+                flexDir="column"
+                justifyContent="center"
+                alignItems="center"
                 h="fit"
                 w="fit"
-                pos="absolute"
+                maxW="1/2"
                 top="10%"
                 right={0}
                 bottom={0}
                 left={0}
-                flexDir="column"
-                justifyContent="center"
-                alignItems="start"
                 p={3}
-                bg="skyblue/50"
                 borderRightRadius="16px"
-                maxW="3/4"
+                xlDown={{ p: 0, maxW: "none" }}
+                mdDown={{ maxW: "1/2" }}
             >
-                <Text fontSize="3xl" color={colors.medium} fontWeight="bolder">
+                <Text
+                    fontSize={{ base: "2xl", lgDown: "xl" }}
+                    textAlign="center"
+                    color={colors.mediumLight}
+                    fontWeight="bolder"
+                >
                     Quality Freshness Guaranteed!
                 </Text>
-                <Text fontSize="xl" textAlign="start" p={3}>
+                <Text
+                    fontSize={{ base: "xl", lgDown: "md" }}
+                    color="white"
+                    textAlign="center"
+                    p={2}
+                >
                     Quickly aggregate empowered networks after emerging
                     products...
                 </Text>
+                <Button
+                    bg={colors.mediumLight}
+                    color="white"
+                    p={2}
+                >
+                    Get It now
+                </Button>
             </Flex>
         </Box>
     )
--- components/carousel/Carousel.component.tsx
+++ components/carousel/Carousel.component.tsx
@@ -2,11 +2,13 @@ import React, { FC } from 'react'
 import { Carousel as CarouselComponent } from "react-responsive-carousel";
 
 import { CarouselProps } from './Carousel';
+import colors from '@/config/colors';
+import { indicatorStyles } from './Carousel.config';
 
 const Carousel: FC<CarouselProps> = ({ children }) => {
     return (
         <CarouselComponent
-            className="min-w-[400px] max-w-[800px]"
+            className="m-2 min-w-[400px] max-w-[800px]"
             autoPlay
             interval={3000}
             transitionTime={1000}
@@ -14,10 +16,42 @@ const Carousel: FC<CarouselProps> = ({ children }) => {
             showArrows={false}
             infiniteLoop
             emulateTouch
+            renderIndicator={renderIndicator}
+            showThumbs={false}
         >
             {children}
         </CarouselComponent>
     )
+
+    function renderIndicator(
+        onClickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void,
+        isSelected: boolean,
+        index: number,
+        label: string
+    ) {
+        if (isSelected) {
+            return (
+                <li
+                    style={{ ...indicatorStyles, background: colors.medium }}
+                    aria-label={`Selected: ${label} ${index + 1}`}
+                    title={`Selected: ${label} ${index + 1}`}
+                />
+            );
+        }
+        return (
+            <li
+                style={indicatorStyles}
+                onClick={onClickHandler}
+                onKeyDown={onClickHandler}
+                value={index}
+                key={index}
+                role="button"
+                tabIndex={0}
+                title={`${label} ${index + 1}`}
+                aria-label={`${label} ${index + 1}`}
+            />
+        );
+    }
 }
 
 export default Carousel
