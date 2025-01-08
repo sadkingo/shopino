d418ee1bf637c53e636a3f751563de7a81875b27
sadkingo <hker0004@gmail.com>
2025-01-08 16:38:17 +0100

    Cleaned

--- components/DiscountItem/DiscountItem.component.tsx
+++ components/DiscountItem/DiscountItem.component.tsx
@@ -1,7 +1,7 @@
 import {FC} from 'react';
 import Image from "next/image";
-import Apple from "@images/apple.png"
-import {Flex} from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import { colors } from "@/config/theme";
 import { DiscountItemProps } from "@components/DiscountItem/DiscountItem";
 
 
