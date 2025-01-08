4dba09707a725f567f53e5f410dc700072c504e0
sadkingo <hker0004@gmail.com>
2025-01-08 03:14:41 +0100

    Exported carousel config

---
+++ components/carousel/Carousel.config.ts
@@ -0,0 +1,12 @@
+import { CSSProperties } from "react";
+
+const indicatorStyles: CSSProperties = {
+  background: "#fff",
+  width: 8,
+  height: 8,
+  display: "inline-block",
+  margin: "0 8px",
+  borderRadius: "50%",
+};
+
+export { indicatorStyles };
