4e9902dcd90a5eeb2fff85de56aa397a78deb0e1
sadkingo <hker0004@gmail.com>
2025-01-08 03:11:44 +0100

    Added grid and carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,41 @@
-import ContactStrap from "@/components/contact-strap/ContactStrap.component";
-import NavBar from "@/components/nav-bar";
+"use client";
+import { Grid, HStack } from "@chakra-ui/react";
+
+import fruit1 from "@/public/images/fruit-1.png";
+import fruit2 from "@/public/images/fruit-2.png";
+import fruit3 from "@/public/images/fruit-3.png";
+
+import CarouselItem from "@/components/carousel-item";
+import Carousel from "@/components/carousel";
+import DiscountCard from "@/components/discount-card";
+import colors from "@/config/colors";
+import DiscountItem from "@components/DiscountItem";
 
 export default function Home() {
   return (
-    <>
-      <ContactStrap />
-      <NavBar />
-    </>
+    <Grid
+      gridTemplateColumns={{ base: "repeat(2, minmax(0, 1fr))", mdDown: "minmax(0, 1fr)" }}
+      h="fit"
+      flexDir={{ lgDown: "column" }}
+      flex={1}
+      p={4}
+    >
+      {renderMainCarousel()}
+      <DiscountCard borderWidth={2} borderColor={colors.light} >
+        <DiscountItem/>
+      </DiscountCard>
+    </Grid>
   );
+
+  function renderMainCarousel() {
+    return (
+      <HStack h="fit">
+        <Carousel>
+          {[fruit1, fruit2, fruit3].map((src, index) => (
+            <CarouselItem key={index} image={src} />
+          ))}
+        </Carousel>
+      </HStack>
+    )
+  }
 }
