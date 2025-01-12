8ffe922da5a6fd185ad0cbfd08613bf92b0515ed
sadkingo <hker0004@gmail.com>
2025-01-12 17:07:33 +0100

    Adjusted layout css

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,28 +8,32 @@ import { Provider } from "@/components/ui/provider";
 import Header from "@/components/header";
 import { Box } from "@chakra-ui/react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: process.env.WEBSITE_NAME,
-  description: "Created by " + process.env.COMPANY_NAME,
+    title: process.env.WEBSITE_NAME,
+    description: "Created by " + process.env.COMPANY_NAME,
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} flex flex-col antialiased`} style={{ background: `url(${Background.src})` }}>
+export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} flex flex-col antialiased`} style={{background: `url(${Background.src})`}}>
         <Provider>
-          <Header />
-          <Box as="main" className="m-auto max-w-screen-2xl" bg={{ _light: "whiteAlpha.950", _dark: "blackAlpha.950" }} flex={1} >
-            {children}
-          </Box>
+            <Header/>
+            <Box
+                display="flex"
+                as="main"
+                margin="auto"
+                w="full"
+                maxW="breakpoint-2xl"
+                justifyContent="center"
+                bg={{_light: "whiteAlpha.950", _dark: "blackAlpha.950"}}
+                flex={1}>
+                {children}
+            </Box>
         </Provider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
