c946a58bcf18ef3efaaf785289e03a7ba301f020
sadkingo <hker0004@gmail.com>
2025-01-16 21:50:30 +0100

    fixed folder name

--- components/timer/Timer.container.tsx
+++ components/timer/Timer.container.tsx
@@ -1,12 +1,12 @@
 "user client";
 import { FC } from 'react';
 import Timer from "./Timer.component";
-import { TimerProps } from "@components/Timer/Timer";
+import { TimerProps } from "@components/timer/Timer";
 
 const TimerContainer: FC<TimerProps> = ({date}) => {
-    return (
-        <Timer date={date}/>
-    )
+  return (
+    <Timer date={date}/>
+  )
 }
 
 export default TimerContainer;
