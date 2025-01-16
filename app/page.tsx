ab4eb021816aea7c8eb1edcbe88d76052194acf2
sadkingo <hker0004@gmail.com>
2025-01-16 21:45:05 +0100

    extracted view for better readability

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Flex, Grid, GridItem } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, Link, Text, VStack } from "@chakra-ui/react";
 
 import fruit1 from "@/public/images/fruit-1.png";
 import fruit2 from "@/public/images/fruit-2.png";
@@ -10,54 +10,62 @@ import CarouselItem from "@/components/carousel-item";
 import Carousel from "@/components/carousel";
 import DiscountCard from "@/components/discount-card";
 import colors from "@/config/colors";
-import DiscountItem from "@components/DiscountItem";
+import DiscountItem from "../components/discount-item";
+import { Alert } from "@ui/alert";
+import { themedColors } from "@/config/theme";
 
 export default function Home() {
     const date = new Date(2026, 2, 15, 3, 4, 5);
 
-  return (
-    <Grid
-      gridTemplateColumns="repeat(12, minmax(0, 1fr))"
-      flexDir={{ lgDown: "column" }}
-      h="fit"
-      w="fit"
-      p={4}
-      flex={1}
-      gapY={8}
-    >
-      <GridItem display="flex" alignItems="center" colSpan={{ base: 7, lgDown: 12 }}>
-        {renderMainCarousel()}
-      </GridItem>
-      <GridItem colSpan={{ base: 5, lgDown: 12 }}>
-        {renderCouponCard()}
-      </GridItem>
-      <GridItem colSpan={12}>
-        <Alert
-          icon={<></>}
-          status="info"
-          variant="subtle"
+    return (
+        <Grid
+            gridTemplateColumns="repeat(12, minmax(0, 1fr))"
+            flexDir={{lgDown: "column"}}
+            h="fit"
+            w="fit"
+            p={4}
+            flex={1}
+            gapY={8}
         >
-          <VStack
-            gap={4}
-          >
-            <Flex
-              fontSize="2xl"
-              color={themedColors.light}
+            <GridItem display="flex" alignItems="center" colSpan={{base: 7, lgDown: 12}}>
+                {renderMainCarousel()}
+            </GridItem>
+            <GridItem colSpan={{base: 5, lgDown: 12}}>
+                {renderCouponCard()}
+            </GridItem>
+            <GridItem colSpan={12}>
+                {renderDiscountAlert()}
+            </GridItem>
+        </Grid>
+    );
+
+    function renderDiscountAlert() {
+        return (
+            <Alert
+                icon={<></>}
+                status="info"
+                variant="subtle"
             >
-              100% Natural Quality Organic Product
-            </Flex>
-            <Text
-              fontSize="xl"
-              gap={2}
-            >
-              See Our latest discounted products from here and get a special
-              <Link m={2} color={themedColors.medium} href="#Products">discount product</Link>
-            </Text>
-          </VStack>
-        </Alert>
-      </GridItem>
-    </Grid>
-  );
+                <VStack
+                    gap={4}
+                >
+                    <Flex
+                        fontSize="2xl"
+                        color={themedColors.light}
+                    >
+                        100% Natural Quality Organic Product
+                    </Flex>
+                    <Text
+                        fontSize="xl"
+                        gap={2}
+                    >
+                        See Our latest discounted products from here and get a special
+                        <Link m={2} color={themedColors.medium} href="#Products">discount product</Link>
+                    </Text>
+                </VStack>
+            </Alert>
+        )
+    }
 
     function renderMainCarousel() {
         return (
