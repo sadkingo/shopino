516158d311483846d387a1f91e827cddf7609f64
sadkingo <hker0004@gmail.com>
2025-01-19 01:32:21 +0100

    Changed the name of the main div to main

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,28 +6,29 @@ import "./globals.css";
 import { Provider } from "@/components/ui/provider";
 import Header from "@/components/header";
 import { Box } from "@chakra-ui/react";
+import Footer from "@components/footer";
 
-const inter = Inter({ subsets: [ "latin" ] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: process.env.WEBSITE_NAME,
   description: "Created by " + process.env.COMPANY_NAME,
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en" suppressHydrationWarning>
     <body className={`${inter.className} flex flex-col antialiased`}>
     <Provider>
       <Header/>
       <Box
-        display="flex"
         as="main"
+        display="flex"
         margin="auto"
         w="full"
         maxW="breakpoint-2xl"
         justifyContent="center"
-        bg={{ _light: "whiteAlpha.950", _dark: "blackAlpha.950" }}
+        bg={{_light: "whiteAlpha.950", _dark: "blackAlpha.950"}}
         flex={1}>
         {children}
       </Box>
