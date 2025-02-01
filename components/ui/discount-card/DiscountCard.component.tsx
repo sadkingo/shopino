95273e2c0a994bfc1e90abf3ea25dbda3fdf4731
sadkingo <hker0004@gmail.com>
2025-02-01 02:04:52 +0100

    Changed paths

--- components/ui/discount-card/DiscountCard.component.tsx
+++ components/ui/discount-card/DiscountCard.component.tsx
@@ -2,28 +2,28 @@ import React, { FC } from 'react'
 import { Text, VStack } from '@chakra-ui/react'
 
 import { DiscountCardProps } from './DiscountCard'
-import colors from '@/config/colors'
+import colors from '@/lib/theme-config/colors'
 
 const DiscountCard: FC<DiscountCardProps> = ({children, ...restProps}) => {
-    return (
-        <VStack
-            gap={0}
-            h="full"
-            w="full"
-        >
-            <Text
-                w="full"
-                bg={colors.light}
-                p="2"
-                textAlign="center"
-            >
-                Latest Super Discount Active Coupon Code
-            </Text>
-            <VStack w="full" flex={1} {...restProps}>
-                {children}
-            </VStack>
-        </VStack>
-    )
+  return (
+    <VStack
+      gap={0}
+      h="full"
+      w="full"
+    >
+      <Text
+        w="full"
+        bg={colors.light}
+        p="2"
+        textAlign="center"
+      >
+        Latest Super Discount Active Coupon Code
+      </Text>
+      <VStack w="full" flex={1} {...restProps}>
+        {children}
+      </VStack>
+    </VStack>
+  )
 }
 
 export default DiscountCard
