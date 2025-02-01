d4c7f4ccdd703d49292e2a322df8a57fcffb2aa6
sadkingo <hker0004@gmail.com>
2025-02-01 02:04:03 +0100

    Cleaned up

--- components/ui/carousel/Carousel.component.tsx
+++ components/ui/carousel/Carousel.component.tsx
@@ -2,56 +2,56 @@ import React, { FC } from 'react'
 import { Carousel as CarouselComponent } from "react-responsive-carousel";
 
 import { CarouselProps } from './Carousel';
-import colors from '@/config/colors';
+import colors from '@/lib/theme-config/colors';
 import { indicatorStyles } from './Carousel.config';
 
 const Carousel: FC<CarouselProps> = ({children}) => {
-    return (
-        <CarouselComponent
-            className="flex-1"
-            autoPlay
-            interval={3000}
-            transitionTime={1000}
-            showStatus={false}
-            showArrows={false}
-            infiniteLoop
-            emulateTouch
-            renderIndicator={renderIndicator}
-            showThumbs={false}
-        >
-            {children}
-        </CarouselComponent>
-    )
+  return (
+    <CarouselComponent
+      className="flex-1"
+      autoPlay
+      interval={3000}
+      transitionTime={1000}
+      showStatus={false}
+      showArrows={false}
+      infiniteLoop
+      emulateTouch
+      renderIndicator={renderIndicator}
+      showThumbs={false}
+    >
+      {children}
+    </CarouselComponent>
+  )
 
-    function renderIndicator(
-        onClickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void,
-        isSelected: boolean,
-        index: number,
-        label: string,
-    ) {
-        if (isSelected) {
-            return (
-                <li
-                    style={{...indicatorStyles, background: colors.medium}}
-                    aria-label={`Selected: ${label} ${index + 1}`}
-                    title={`Selected: ${label} ${index + 1}`}
-                />
-            );
-        }
-        return (
-            <li
-                style={indicatorStyles}
-                onClick={onClickHandler}
-                onKeyDown={onClickHandler}
-                value={index}
-                key={index}
-                role="button"
-                tabIndex={0}
-                title={`${label} ${index + 1}`}
-                aria-label={`${label} ${index + 1}`}
-            />
-        );
+  function renderIndicator(
+    onClickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void,
+    isSelected: boolean,
+    index: number,
+    label: string,
+  ) {
+    if (isSelected) {
+      return (
+        <li
+          style={{...indicatorStyles, background: colors.medium}}
+          aria-label={`Selected: ${label} ${index + 1}`}
+          title={`Selected: ${label} ${index + 1}`}
+        />
+      );
     }
+    return (
+      <li
+        style={indicatorStyles}
+        onClick={onClickHandler}
+        onKeyDown={onClickHandler}
+        value={index}
+        key={index}
+        role="button"
+        tabIndex={0}
+        title={`${label} ${index + 1}`}
+        aria-label={`${label} ${index + 1}`}
+      />
+    );
+  }
 }
 
 export default Carousel
