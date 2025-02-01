77e211eeef956addfb1810d7c87fd4b9f0e27823
sadkingo <hker0004@gmail.com>
2025-02-01 02:39:13 +0100

    Fixed type import

--- components/ui/discount-item/DiscountItem.container.tsx
+++ components/ui/discount-item/DiscountItem.container.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
 import DiscountItem from "./DiscountItem.component";
-import { DiscountItemProps } from "@components/discount-item/DiscountItem";
+import { DiscountItemProps } from "@ui/discount-item/DiscountItem";
 
 const DiscountItemContainer: FC<DiscountItemProps> = ({image, coupon, date, ...restProps}) => {
-    return (
-        <DiscountItem image={image} coupon={coupon} date={date} {...restProps}/>
-    )
+  return (
+    <DiscountItem image={image} coupon={coupon} date={date} {...restProps}/>
+  )
 }
 
 export default DiscountItemContainer;
